9d0787740bc19d1c96a725181873bf421af861cc
Peter Tascio <peter.tascio@gmail.com>
2016-08-22 19:12:38 +0000

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,29 @@ function moveDodgerLeft() {
   var left = parseInt(leftNumbers, 10)
 
   if (left > 0) {
-  dodger.style.left = `${left - 1}px`
+  dodger.style.left = `${left - 5}px`
+}
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left < 360) {
+  dodger.style.left = `${left + 5}px`
 }
 }
 
 //why won't this function work? I added a right attrivute to the dodger id
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-
-  if (right < 360) {
-    dodger.style.right = `${right + 1}px`
-  }
-}
+// function moveDodgerRight() {
+//   var rightNumbers = dodger.style.right.replace('px', '')
+//   var right = parseInt(rightNumbers, 10)
+//   console.log(dodger.style.right);
+//
+//   if (right < 360) {
+//     dodger.style.right = `${right + 1}px`
+//   }
+// }
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {

52ea4c51f8bd22b120ce9c5a402e4c682884810a
Shaw Li <shaw.s.li@gmail.com>
2016-09-05 17:29:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`
+    }
+  }
+
+
+function moveDodgerRight() {
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 39: var leftNumbers = dodger.style.left.replace('px', '')
+        var left = parseInt(leftNumbers, 10)
+        if (left <360) {
+          dodger.style.left = `${left + 10}px`
+        }
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  })
+}
+
+$(document).ready(function(){
+  document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+    moveDodgerLeft()
+    }
+  })
+
+  moveDodgerRight();
+})

bbaea9d13ab2859d17174f90af5329bba4a924ec
Niti Nrw <niti.patel@flatironschool.com>
2018-10-10 16:33:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+dodger.style.right = '100px'
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  console.log('rightNumbers is ', rightNumbers);
+  console.log(right);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`
+  }
+}

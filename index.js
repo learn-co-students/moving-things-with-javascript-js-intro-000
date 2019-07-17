1d3dc7d6859bd76c8921868017e1336cf80631b8
James Ellis <13jellis@toothillschool.co.uk>
2019-07-17 10:44:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+
+document.addEventListener('keydown', function(key) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = '${left - 1}px'
+  }
+})

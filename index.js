86ea335467c7309636ca588f838666a1bb109555
Sunny Niu <sunnyyingli@gmail.com>
2019-09-02 04:54:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+//let dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+
+document.addEventListener('keydown', function(e){
+  let leftNumber = dodger.style.left.replace('px','');
+  let number = parseInt(leftNumber, 10);
+
+  if(e.which === 37){
+    moveDodgerLeft(number)
+  }
+
+  if(e.which === 39){
+    moveDodgerRight(number)
+  }
+})
+
+function moveDodgerLeft(number){
+  dodger.style.left = `${number-1}px`;
+}
+
+function moveDodgerRight(number){
+  dodger.style.left = `${number+1}px`;
+}

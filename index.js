b1c1f8e6d4e48abb75338d686a417c84f5fdb5d1
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-06 00:47:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#000000"
+dodger.style.backgroundColor = '#FF69B4'
+
+console.log(dodger.style.left)
+console.log(dodger.style.bottom)
+
+dodger.style.bottom = '100px'
+dodger.style.bottom = '0px'
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left - 50}px`
+    }
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowRight') {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    if (left < 350) {
+      dodger.style.left = `${left + 50}px`
+    }
+  }
+})

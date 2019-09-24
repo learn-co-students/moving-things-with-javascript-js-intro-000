941a7a1b801ad4d9013939ebe655ceebcdaf65ff
Abhishek Jadhav <abhidj0001@gmail.com>
2019-09-24 06:32:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const app = "I don't do much."
-fdvv
+
+
+var dodger = document.getElementById('dodger')
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})

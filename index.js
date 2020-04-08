5b5536a92acbdedd556c057391505c46063c9d08
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-08 10:37:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px','')
+    var left = parseInt(leftNumbers, 10);
+if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
+}
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px','')
+    var left = parseInt(leftNumbers, 10);
+if (left < 360) {
+    dodger.style.left = `${left + 5}px`;
+}
+}
+
+document.addEventListener('keydown', function(key) {
+    if (key.which == 37) {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener('keydown', function(key) {
+    if (key.which == 39) {
+        moveDodgerRight()
+    }
+})

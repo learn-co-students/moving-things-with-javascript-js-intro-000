e15bcdd9873df9ed77caae950d80249acdda4377
Scott Mittman <sm2884@nau.edu>
2020-06-15 03:04:29 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ document.addEventListener('keydown', function(e) {
     moveDodgerLeft()
   }
 })
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace('px','');
+  var right = parseInt(rightNumbers,10);
+  if (right>0){
+    dodger.style.right = `${right-1}px`
+  }
+}
+document.addEventListener('keydown',function(e) {
+  if (e.which===39){
+    moveDodgerRight();
+  }
+})

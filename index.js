5862612d96865e01472f675f08c27b35f1d81b44
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-22 00:00:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+//65 is A, 68 is D.
+
+document.addEventListener('keydown', (e)=>{
+  // stuff doesn't move if i put this outside.
+  // doesn't matter that it has to be outside other than console inspection.
+  var dodger = document.getElementById('dodger');
+  var width = parseInt($("#dodger").css("width").replace('px',''), 10);
+    // for some reason "dodger.style.width" doesn't access it.
+    // "dodger.style.right" gives null value, doesn't autocalc.
+  var leftEdge = parseInt(dodger.style.left.replace('px',''), 10)
+  var rightEdge = leftEdge + width;
+  var rightFrame = parseInt($("#game").css("width").replace('px',''), 10);
+
+  if (e.which === 65 && leftEdge > 0){
+    console.log("moving left");
+    dodger.style.left= `${leftEdge - 5}px`;
+  } else if (e.which === 68 && rightEdge < rightFrame){
+    console.log("moving right");
+    dodger.style.left= `${leftEdge + 5}px`;
+  }
+})

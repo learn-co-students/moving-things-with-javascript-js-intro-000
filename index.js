4acc7d7ec06dafccb5682b2d2e9d16d7004a8b7a
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-29 17:46:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+document.addEventListener('keydown', function(e) {
+ console.log(e.which)
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left - 1}px`
+  }
+})

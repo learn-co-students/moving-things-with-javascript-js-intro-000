336c4ff7c470d316bd309a8cd1a3113f216887b0
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 17:05:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = '#ff69b4'
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
